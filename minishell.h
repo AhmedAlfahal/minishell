c64f00b72c99cfc235888ba219a52aa6ffb75a86
Ahmed Alfahal <111463922+AhmedAlfahal@users.noreply.github.com>
2023-03-30 01:28:00 +0400

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -16,10 +16,41 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include <limits.h>
-# include <pthread.h>
-# include <sys/time.h>
 
+typedef struct s_counters
+{
+	int	pipes;
+	int	d_cot;
+	int	s_cot;
+	int	l_redir;
+	int	r_redir;
+	int	ll_redir;
+	int	rr_redir;
+	int	bslsh;
+	int	smcln;
+}		t_c;
 
+typedef struct s_redirection
+{
+	int		rdr_type;
+	char	*file_name;
+}			t_rdr;
+
+typedef struct s_commands
+{
+	char	**args;
+	t_rdr	redirs;
+}			t_cmd;
+
+typedef struct s_minishell
+{
+	char	**env;
+	char	*rdln;
+	t_cmd	*cmds;
+	int		c_cmds;
+	t_c		*counters;
+	int		error;
+	int		i;
+}			t_ms;
 
 #endif
